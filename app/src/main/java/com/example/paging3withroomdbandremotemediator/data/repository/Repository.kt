11154406059c2d3a8fb77a819a99f3beef90d3c4
AP <ketuimevbore@yahoo.com>
2022-01-08 17:30:12 +0100

    Remote mediator added up to repository level

---
+++ app/src/main/java/com/example/paging3withroomdbandremotemediator/data/repository/Repository.kt
@@ -0,0 +1,32 @@
+package com.example.paging3withroomdbandremotemediator.data.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.paging3withroomdbandremotemediator.data.local.UnsplashDatabase
+import com.example.paging3withroomdbandremotemediator.data.paging.UnsplashRemoteMediator
+import com.example.paging3withroomdbandremotemediator.data.remote.UnSplashApi
+import com.example.paging3withroomdbandremotemediator.model.UnsplashImage
+import com.example.paging3withroomdbandremotemediator.util.Constants.ITEMS_PER_PAGE
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ExperimentalPagingApi
+class Repository @Inject constructor(private val unsplashApi: UnSplashApi, private val unsplashDatabase: UnsplashDatabase) {
+
+    fun getAllImages(): Flow<PagingData<UnsplashImage>>{
+
+        val pagingSourceFactory = { unsplashDatabase.unsplashImageDao().getAllImages() }
+        return Pager(
+            config = PagingConfig(pageSize = ITEMS_PER_PAGE),
+            remoteMediator = UnsplashRemoteMediator(
+                unsplashApi = unsplashApi,
+                unsplashDatabase = unsplashDatabase
+            ),
+            pagingSourceFactory = pagingSourceFactory
+        ).flow
+    }
+
+
+}
